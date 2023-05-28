400436dbf662375eb284394895533ecf736c235a
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-27 20:53:43 -0700

    cards and nav bar

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -10,9 +10,20 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav
-            className='navbar'>
-            <a href='/'>NAVBAR</a>
+        <nav className='navbar'>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+
+
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <a href='/'>NAVBAR</a>
+                </ul>
+            </div>
         </nav>
     );
 }
--- react-portfolio/src/components/Project.js
+++ react-portfolio/src/components/Project.js
@@ -3,151 +3,156 @@ import handle from '../images/handle.png';
 import { CardBody, CardTitle, CardSubtitle, CardText, Button, Card } from 'reactstrap';
 // import { Container, Row, Col } from 'reactstrap';
 import { Container, Row, Col } from 'react-bootstrap';
+import "../App.css";
 
 function Projects() {
     return (
-        <Container fluid className='all-projects'>
-            <div className='main'>
-                <div className='projects'>
-                    <div className='header'>
-                        <h1>Here are my projects</h1>
-                    </div>
-                    <Row xs="2">
-                        <Col md={4} className="bg-dark-border">
-
-                            <Card
-                                color="light"
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <img
-                                    alt="Sample"
-                                    // src="https://picsum.photos/300/200"
-                                    src={handle}
-                                    width='100%'
-                                />
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <Button>
-                                        Button
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md={4} className="bg-dark-border">
-                            <Card
-                                color="light"
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <img
-                                    alt="Sample"
-                                    // src="https://picsum.photos/300/200"
-                                    src={handle}
-                                    width='100%'
-                                />
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <Button>
-                                        Button
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md={4} className="bg-dark-border">
-                            <Card
-                                color="light"
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <img
-                                    alt="Sample"
-                                    // src="https://picsum.photos/300/200"
-                                    src={handle}
-                                    width='100%'
-                                />
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <Button>
-                                        Button
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md={4} className="bg-dark-border">
-                            <Card
-                                color="light"
-                                style={{
-                                    width: '18rem'
-                                }}
-                            >
-                                <img
-                                    alt="Sample"
-                                    // src="https://picsum.photos/300/200"
-                                    src={handle}
-                                    width='100%'
-                                />
-                                <CardBody>
-                                    <CardTitle tag="h5">
-                                        Card title
-                                    </CardTitle>
-                                    <CardSubtitle
-                                        className="mb-2 text-muted"
-                                        tag="h6"
-                                    >
-                                        Card subtitle
-                                    </CardSubtitle>
-                                    <CardText>
-                                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                                    </CardText>
-                                    <Button>
-                                        Button
-                                    </Button>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-
-
-                </div>
+        <Container className='all-projects'>
+            <div className='header'>
+                <h1>Here are my projects</h1>
             </div>
-        </Container>
+
+            <div className='projects'>
+
+                <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                    <Col>
+
+                        <Card
+                            color="light"
+                            style={{
+                                width: '18rem'
+                            }}
+                        >
+                            <img
+                                alt="Sample"
+                                // src="https://picsum.photos/300/200"
+                                src={handle}
+                                width='100%'
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    Card title
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+                                    Card subtitle
+                                </CardSubtitle>
+                                <CardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                </CardText>
+                                <Button>
+                                    Button
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card
+                            color="light"
+                            style={{
+                                width: '18rem'
+                            }}
+                        >
+                            <img
+                                alt="Sample"
+                                // src="https://picsum.photos/300/200"
+                                src={handle}
+                                width='100%'
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    Card title
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+                                    Card subtitle
+                                </CardSubtitle>
+                                <CardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                </CardText>
+                                <Button>
+                                    Button
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+
+                    <Col md={4} className="project-card">
+                        <Card
+                            color="light"
+                            style={{
+                                width: '18rem'
+                            }}
+                        >
+                            <img
+                                alt="Sample"
+                                // src="https://picsum.photos/300/200"
+                                src={handle}
+                                width='100%'
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    Card title
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+                                    Card subtitle
+                                </CardSubtitle>
+                                <CardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                </CardText>
+                                <Button>
+                                    Button
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <Card
+                            color="light"
+                            style={{
+                                width: '18rem'
+                            }}
+                        >
+                            <img
+                                alt="Sample"
+                                // src="https://picsum.photos/300/200"
+                                src={handle}
+                                width='100%'
+                            />
+                            <CardBody>
+                                <CardTitle tag="h5">
+                                    Card title
+                                </CardTitle>
+                                <CardSubtitle
+                                    className="mb-2 text-muted"
+                                    tag="h6"
+                                >
+                                    Card subtitle
+                                </CardSubtitle>
+                                <CardText>
+                                    Some quick example text to build on the card title and make up the bulk of the card‘s content.
+                                </CardText>
+                                <Button>
+                                    Button
+                                </Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+
+
+            </div>
+
+        </Container >
 
     )
 }
