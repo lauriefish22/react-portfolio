3a8ae1ead52261568e4dfcd6e188fe5bdfaa2abb
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-28 13:33:45 -0700

    typo

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 // import Footer from "./components/Footer";
 // import Header from "./components/Header";
 import NavBar from "./components/Navbar";
-import Project from "./components/Project";
+import Project from "./components/ProjectContainer";
 import './App.css';
 
 
