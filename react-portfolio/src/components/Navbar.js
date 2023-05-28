c55b8a83c877b0d66904218bdba26afe929eda96
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-28 13:31:04 -0700

    changed files

--- react-portfolio/src/components/Navbar.js
+++ react-portfolio/src/components/Navbar.js
@@ -6,12 +6,12 @@ function Header({ currentPage, handlePageChange }) {
         <ul className='nav nav-tabs' id='navbar'>
             <li className='nav-item'>
 
-                <a href="#home" onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}>
+                <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
                     Home</a>
             </li>
             <li className='nav-item'>
-                <a href="#about" onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
-                    About Me</a>
+                <a href="#project" onClick={() => handlePageChange('Project')} className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}>
+                    Project</a>
             </li>
         </ul>
     )
