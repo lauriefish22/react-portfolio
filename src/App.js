b2d1cf63f122c0d4302f68a7bd953bc08116ea54
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-29 21:42:04 -0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-import PortfolioContainer from "./components/PortfolioContainer.js";
-import './style.css';
+import PortfolioContainer from "./components/PortfolioContainer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+
+
 
 function App() {
     return (
