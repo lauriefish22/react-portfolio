fc273f857283ea3a48716d2b6c2491b91ca72f11
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-29 21:50:07 -0700

    fixing

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import PortfolioContainer from "./components/PortfolioContainer.js";
+
+function App() {
+    return (
+        <div className='portfolio'>
+            <PortfolioContainer />
+        </div>
+    );
+}
+export default App;
