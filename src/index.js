705d55aa68911bde7d75ce06dd3362a0ea37ab76
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-05-29 22:01:44 -0700

    fixed

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
